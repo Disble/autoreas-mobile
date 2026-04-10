98d324f6b7e2317820630adc9830d7f1b4168c5c
disble <contact@disble.com>
2026-04-09 21:59:30 -0500

    fix: wrap main layout in SafeAreaView for better UI consistency

--- src/features/setup/ui/AppRootLayout/AppRootLayout.tsx
+++ src/features/setup/ui/AppRootLayout/AppRootLayout.tsx
@@ -3,6 +3,7 @@ import { HeroUINativeProvider } from 'heroui-native';
 import { Suspense } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { SQLiteUnavailableScreen } from '../../../../components/sqlite-unavailable-screen';
 import { AppThemeProvider } from '../../../../contexts/app-theme-context';
 import type { AppRootLayoutProps } from './app-root-layout.types';
@@ -29,31 +30,33 @@ export function AppRootLayout(props: AppRootLayoutProps) {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <KeyboardProvider>
-        <Suspense fallback={null}>
-          <SQLiteProvider
-            databaseName={databaseName}
-            onInit={handleDatabaseInit}
-            options={sqliteOptions}
-            useSuspense
-          >
-            <AppThemeProvider>
-              <HeroUINativeProvider
-                config={{
-                  textProps: {
-                    maxFontSizeMultiplier: 2,
-                  },
-                  toast: {
-                    contentWrapper,
-                  },
-                }}
-              >
-                {bootState.initialized ? <Slot /> : null}
-              </HeroUINativeProvider>
-            </AppThemeProvider>
-          </SQLiteProvider>
-        </Suspense>
-      </KeyboardProvider>
+      <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom']}>
+        <KeyboardProvider>
+          <Suspense fallback={null}>
+            <SQLiteProvider
+              databaseName={databaseName}
+              onInit={handleDatabaseInit}
+              options={sqliteOptions}
+              useSuspense
+            >
+              <AppThemeProvider>
+                <HeroUINativeProvider
+                  config={{
+                    textProps: {
+                      maxFontSizeMultiplier: 2,
+                    },
+                    toast: {
+                      contentWrapper,
+                    },
+                  }}
+                >
+                  {bootState.initialized ? <Slot /> : null}
+                </HeroUINativeProvider>
+              </AppThemeProvider>
+            </SQLiteProvider>
+          </Suspense>
+        </KeyboardProvider>
+      </SafeAreaView>
     </GestureHandlerRootView>
   );
 }
