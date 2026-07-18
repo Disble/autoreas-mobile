91f91f6106c287f32ab8183643b2dcacd198a6ca
disble <contact@disble.com>
2026-07-17 21:12:27 -0500

    fix(startup): prepare splash screen during root-layout module initialization

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import '../../global.css';
+import '../features/setup/ui/AppRootLayout/app-root-layout.startup';
 import { AppRootLayout } from '../features/setup/ui/AppRootLayout';
 
 /** Renders the layout interface. */
